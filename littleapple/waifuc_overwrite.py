f48bdbacb2be045642da309a74643ae85c67a96f
LittleApple-fp16 <148928493+LittleApple-fp16@users.noreply.github.com>
2023-11-15 21:34:58 +0800

    fix waifuc bug

--- littleapple/waifuc_overwrite.py
+++ littleapple/waifuc_overwrite.py
@@ -6,6 +6,44 @@ from waifuc.export.base import LocalDirectoryExporter
 from waifuc.model import ImageItem
 
 from waifuc.export import TextualInversionExporter
+from waifuc.source.web import WebDataSource
+from shutil import rmtree
+from typing import Iterator, Tuple, Union
+from PIL import UnidentifiedImageError, Image
+from PIL.Image import DecompressionBombError
+from hbutils.system import urlsplit, TemporaryDirectory
+import warnings
+from waifuc.utils import download_file
+class AppleWebDataSource(WebDataSource):
+    def _iter(self) -> Iterator[ImageItem]:
+        for id_, url, meta in self._iter_data():
+            with TemporaryDirectory() as td:
+                _, ext_name = os.path.splitext(urlsplit(url).filename)
+                filename = f'{self.group_name}_{id_}{ext_name}'
+                td_file = os.path.join(td, filename)
+                try:
+                    download_file(
+                        url, td_file, desc=filename,
+                        session=self.session, silent=self.download_silent
+                    )
+                    image = Image.open(td_file)
+                    image.load()
+                except UnidentifiedImageError:
+                    warnings.warn(f'{self.group_name.capitalize()} resource {id_} unidentified as image, skipped.')
+                    continue
+                except (IOError, DecompressionBombError) as err:
+                    warnings.warn(f'Skipped due to error: {err!r}')
+                    continue
+                except NotADirectoryError:
+                    os.remove(td)
+                    continue
+
+                meta = {**meta, 'url': url}
+                yield ImageItem(image, meta)
+
+
+
+
 
 
 class AppleTextualInversionExporter(TextualInversionExporter):
