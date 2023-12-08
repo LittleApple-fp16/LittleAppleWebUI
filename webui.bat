de2cddb59d26b42f7addd2d70bbb5277c94c0235
LittleApple-fp16 <148928493+LittleApple-fp16@users.noreply.github.com>
2023-12-08 14:13:38 +0800

    add: detect cuda version

--- webui.bat
+++ webui.bat
@@ -3,6 +3,7 @@
 title=LittleAppleWebui
 SET VENV_NAME=venv
 SET PYTHON=python
+SET CUDA_VERSION=
 
 echo [info] Auto update...
 git pull
@@ -15,7 +16,25 @@ if not exist %VENV_NAME% (
     set "path=%cd%\%VENV_NAME%\scripts;%cd%\%VENV_NAME%\dep\python;%cd%\%VENV_NAME%\dep\python\scripts;%cd%\%VENV_NAME%\dep\git\bin;%cd%;%path%"
     echo [init] Installing deps...
     python -m pip install --upgrade pip
-    python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+    
+    FOR /F "tokens=*" %%i IN ('nvcc -V ^| findstr /C:"release"') DO SET CUDA_VERSION=%%i
+    IF "%CUDA_VERSION%"=="" (
+        echo CUDA is not installed or nvcc is not in your PATH.
+    ) ELSE (
+        FOR /F "tokens=5" %%a IN ("%CUDA_VERSION%") DO SET CUDA_VERSION=%%a
+        echo Detected CUDA Version: %CUDA_VERSION%
+        IF "%CUDA_VERSION%"=="11.8" (
+            echo Installing PyTorch for CUDA 11.8...
+            python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+        ) ELSE IF "%CUDA_VERSION%"=="12.1" (
+            echo Installing PyTorch for CUDA 12.1...
+            python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+        ) ELSE (
+            echo Unsupported CUDA Version. Please contact the developer.
+        )
+    )
+
     python -m pip install xformers==0.0.22 --no-deps
     pip install -r requirements.txt
     echo [info] Done. Please restart.
